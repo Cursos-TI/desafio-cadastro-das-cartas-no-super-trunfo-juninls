29bf6b7248bf5cbb986704bb573e70c7df080a30
juninls <junin_ls@hotmail.com>
2025-08-24 16:17:56 +0000

    niv

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    char codigo1[4], codigo2[4];
-    int populacao1, populacao2;
-    float area1, area2;
-    float pib1, pib2;
-    int pontos1, pontos2;
+    // Variáveis da carta 1
+    char codigo1[4];
+    int populacao1;
+    float area1;
+    float pib1;
+    int pontos1;
 
-    // Carta 1
+    // Variáveis da carta 2
+    char codigo2[4];
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontos2;
+
+    // Entrada de dados da primeira carta
     printf("Digite o código da primeira carta (ex: A01): ");
     scanf("%3s", codigo1);
 
@@ -23,10 +31,8 @@ int main() {
     printf("Digite o número de pontos turísticos da primeira cidade: ");
     scanf("%d", &pontos1);
 
-    printf("\n");
-
-    // Carta 2
-    printf("Digite o código da segunda carta (ex: B02): ");
+    // Entrada de dados da segunda carta
+    printf("\nDigite o código da segunda carta (ex: B02): ");
     scanf("%3s", codigo2);
 
     printf("Digite a população da segunda cidade: ");
@@ -41,18 +47,17 @@ int main() {
     printf("Digite o número de pontos turísticos da segunda cidade: ");
     scanf("%d", &pontos2);
 
-    // Exibe as cartas cadastradas
-    printf("\n--- Cartas cadastradas ---\n");
-
-    printf("Carta 1: %s\n", codigo1);
+    // Exibição dos dados da primeira carta
+    printf("\n--- Carta 1 ---\n");
+    printf("Código: %s\n", codigo1);
     printf("População: %d\n", populacao1);
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões\n", pib1);
     printf("Pontos turísticos: %d\n", pontos1);
 
-    printf("\n");
-
-    printf("Carta 2: %s\n", codigo2);
+    // Exibição dos dados da segunda carta
+    printf("\n--- Carta 2 ---\n");
+    printf("Código: %s\n", codigo2);
     printf("População: %d\n", populacao2);
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões\n", pib2);
